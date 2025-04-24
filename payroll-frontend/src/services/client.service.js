1b2269066bcf0b1ee36d34c7f61014be2c35b520
ThomasR <thomas.riffo@usach.cl>
2025-04-24 08:29:17 -0400

    Arregle el problema de CORS

--- payroll-frontend/src/services/client.service.js
+++ payroll-frontend/src/services/client.service.js
@@ -1,22 +1,12 @@
 import httpClient from "../http-common";
 
 const getAll = () => {
-    return httpClient.get('/api/v1/client/');
+    return httpClient.get('/api/v1/clients/');
 }
 
 const create = data => {
-    return httpClient.post("/api/v1/client/", data);
+    return httpClient.post("/api/v1/clients/", data);
 }
 
-const get = id => {
-    return httpClient.get(`/api/v1/employees/${id}`);
-}
 
-const update = data => {
-    return httpClient.put('/api/v1/employees/', data);
-}
-
-const remove = id => {
-    return httpClient.delete(`/api/v1/employees/${id}`);
-}
-export default { getAll, create, get, update, remove };
+export default { getAll, create};
